5f3f9e66fb7a04e7df08fd5a9a0825fe18fed73f
yskong <ykon0029@student.monash.edu>
2024-10-18 02:40:32 +1100

    “deploy”

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // base: process.env.NODE_ENV === 'production' ? '/FIT5032_efolio/' : '/'  // <-- Add this line
+  base: process.env.NODE_ENV === 'production' ? '/FIT5032_efolio/' : '/'  // <-- Add this line
 })
