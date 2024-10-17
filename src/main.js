b0e4c545b5e8edb3d9bb51b975e01fd53b7b23a1
yskong <ykon0029@student.monash.edu>
2024-10-18 02:08:58 +1100

    “deploy”

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import './style.css'
+import './style.css'
 
 // import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css' 
