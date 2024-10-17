0c70d350f82e82368fc2dcac53c866a434e9ae6f
yskong <ykon0029@student.monash.edu>
2024-10-18 02:30:23 +1100

    “deploy”

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueDevTools()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // base: process.env.NODE_ENV === 'production' ? '/FIT5032_efolio/' : '/'  // <-- Add this line
+  // Fix: Added a comma before the base option
+  // base: process.env.NODE_ENV === 'production' ? '/FIT5032-efolio-moco/' : '/'
 })
